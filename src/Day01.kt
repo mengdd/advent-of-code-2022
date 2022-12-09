32e31f5b1dde5ef5c549580f55167c6a4e7e0291
Dandan Meng <mengdandanno1@163.com>
2022-12-09 20:20:21 +0800

    day 1: part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,24 @@
+import java.lang.Integer.max
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxSum = 0
+        var groupSum = 0
+        for (line in input) {
+            if (line.isBlank() || line.isEmpty()) {
+                maxSum = max(groupSum, maxSum)
+                groupSum = 0
+            } else {
+                groupSum += line.toInt()
+            }
+        }
+        // 66306
+        return max(groupSum, maxSum)
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
